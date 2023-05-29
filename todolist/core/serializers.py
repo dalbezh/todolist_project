90664bb57153aff1f10059b565baa91f905ec8df
dalbezh <dalbezh@icloud.com>
2023-05-29 23:47:55 +0300

    added to readme

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -5,8 +5,6 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from core.exceptions import AuthenticationFailedRu
-
 User = get_user_model()
 
 
